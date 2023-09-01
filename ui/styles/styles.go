058cd6db171b5e79e01fcf0308897e62c73e9ed0
Kevin Ard <kard@quantummetric.com>
2023-09-01 12:57:47 -0400

    feat(ui): more robust console helpers

--- ui/styles/styles.go
+++ ui/styles/styles.go
@@ -2,8 +2,10 @@
 package styles
 
 import (
+	"fmt"
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/lipgloss"
+	"log"
 )
 
 // special text styles.
@@ -16,16 +18,64 @@ var (
 // basic text styles.
 var (
 	OKTextStyle      = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#9342f5"))
+	AltTextStyle     = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#4272f5"))
 	WarningTextStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#ffff00"))
 	ErrorTextStyle   = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#f54542"))
 	FatalTextStyle   = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#e31959")).Bold(true)
 )
 
 // text helpers.
-func OKText(message string) string    { return OKTextStyle.Render("> " + message) }
-func WarnText(message string) string  { return WarningTextStyle.Render("⚠ " + message) }
-func ErrorText(message string) string { return ErrorTextStyle.Render("‼ " + message) }
-func FatalText(message string) string { return FatalTextStyle.Render("X️ " + message) }
+func OKTextf(message string, v ...any) string {
+	return OKTextStyle.Render(fmt.Sprintf("> "+message, v...))
+}
+func AltTextf(message string, v ...any) string {
+	return AltTextStyle.Render(fmt.Sprintf(message, v...))
+}
+func WarnTextf(message string, v ...any) string {
+	return WarningTextStyle.Render(fmt.Sprintf("⚠ "+message, v...))
+}
+func ErrorTextf(message string, v ...any) string {
+	return ErrorTextStyle.Render(fmt.Sprintf("‼ "+message, v...))
+}
+func FatalTextf(message string, v ...any) string {
+	return FatalTextStyle.Render(fmt.Sprintf("X "+message, v...))
+}
+
+type Level int
+
+const (
+	OKLevel Level = iota
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+)
+
+// Println passes one of the leveled formatters to fmt.Println.
+// if level is FatalLevel, it additionally calls log.Fatalln
+func Println(level Level, message string, v ...any) {
+	var line string
+
+	switch level {
+	case WarnLevel:
+		line = WarnTextf(message, v...)
+
+	case ErrorLevel:
+		line = ErrorTextf(message, v...)
+
+	case FatalLevel:
+		line = FatalTextf(message, v...)
+
+	case OKLevel:
+		fallthrough
+	default:
+		line = OKTextf(message, v...)
+	}
+
+	fmt.Println(line)
+	if level == FatalLevel {
+		log.Fatalln("exiting")
+	}
+}
 
 // component styles.
 var (
